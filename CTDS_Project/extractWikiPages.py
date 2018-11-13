4dffdcbabf8d320c16f03b997a8d5da39d441341
Selametia <andreas.hjort@live.dk>
2018-11-13 15:23:14 +0100

    added data extraction script + data

---
+++ CTDS_Project/extractWikiPages.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 13:29:40 2018
+
+@author: Andreas Hjort
+"""
+
+
+from urllib.request import urlopen
+import json
+import pandas as pd
+import io
+
+baseurl = "https://en.wikipedia.org/w/api.php?"
+action = "action=query"
+content = "prop=extracts&exlimit=max&explaintext"
+rvprop ="rvprop=timestamp|content"
+dataformat = "format=json"
+rvdir = "rvdir=older" #sort revisions from newest to oldest
+start = "rvend=2016-01-03T00:00:00Z" #start of my time period
+end = "rvstart=2020-01-03T00:00:00Z" #end of my time period
+limit = "rvlimit=1" #consider only the first revision
+wikiresponse = {}
+
+
+database = pd.read_csv('WikiDB.csv',encoding="utf-8")
+
+members = database['WikiPageName'].unique()
+counter = 1
+for member in members:
+    title = "titles=" + member
+    print(str(counter) + "/100 files downloaded")
+    query = "%s%s&%s&%s&%s&%s&%s&%s&%s&%s" % (baseurl, action, title, content, rvprop, dataformat, rvdir, end, start, limit)
+    wikisource = urlopen(query)
+    w = wikisource.read()
+    wikiJSON = json.loads(w)
+    d = wikiJSON['query']['pages']
+    text = d[str(list(d.keys())[0])]['extract']
+    toWrite = text.encode('utf-8').decode('unicode_escape')
+    
+    #file = open('Democratic.txt', 'a+')
+    file = io.open("Wikipages/" + member + ".txt", mode="w", encoding="utf-8")
+    file.write(toWrite)
+    file.close()
+    counter +=1
