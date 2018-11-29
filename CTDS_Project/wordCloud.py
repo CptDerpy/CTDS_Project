106e2a1ad60be5f9bdad0e94088305244978ef9d
Selametia <andreas.hjort@live.dk>
2018-11-29 12:12:24 +0100

    wordCloud

---
+++ CTDS_Project/wordCloud.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 11:41:42 2018
+
+@author: Andreas Hjort
+"""
+
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords 
+
+class wordCloud:
+    
+    def __init__(self, filename):
+        self.filename = filename
+        self.filetext = self.loadFile(filename)
+        self.makeCloud(self.filetext)
+        
+    
+    def loadFile(self, filename):
+        file = open(filename, encoding="utf8")
+        return file.read()
+    
+    def makeCloud(self, fileText):
+        
+        stopwordslist = set(stopwords.words('english'))
+        tokenizer = RegexpTokenizer(r'\w+')
+        token = tokenizer.tokenize(fileText)
+        tokens = [w for w in token if w not in stopwordslist]
+        count = Counter(tokens)
+        countTF =  Counter({k:float(v)/float(len(fileText)) for k,v in count.items()})
+        wordcloud = WordCloud(background_color='white')
+        wordcloud.generate_from_frequencies(frequencies=countTF)
+        plt.figure()
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis("off")
+        plt.show()
