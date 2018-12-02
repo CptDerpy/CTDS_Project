1ca9f258f7be642ed25c630a6b2bb0de26e834de
CptDerpy <august.birk.olsen@gmail.com>
2018-12-02 01:18:43 +0100

    Cleaned up main script

--- CTDS_Project/main.py
+++ CTDS_Project/main.py
@@ -1,15 +1,17 @@
+import os, sys
+
 from database import * # pylint: disable=W0614
 from wordCloud import * # pylint: disable=W0614
-import os, sys
+
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy
 from PyQt5.QtWidgets import QPushButton, QMainWindow, QLineEdit, QLabel 
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView
-import matplotlib
-matplotlib.use('Qt5Agg')
+
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+
 
 # Paths to test data and wikipedia articles
 path = os.path.dirname(os.path.abspath(__file__))
@@ -20,7 +22,7 @@ test_path = os.path.join(path, 'TestOpgaver')
 # Show wordclouds on GUI
 class PlotCanvas(FigureCanvas):
  
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, parent=None, width=4, height=1, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
@@ -33,7 +35,6 @@ class PlotCanvas(FigureCanvas):
         self.top_plt.set_title('Input document')
         self.btm_plt = self.figure.add_subplot(212)
         self.btm_plt.axis("off")
-        self.btm_plt.set_title('Wikipedia article')
 
     def plot_top(self, file):
         test_file = os.path.join(test_path, file)
@@ -42,9 +43,10 @@ class PlotCanvas(FigureCanvas):
         self.top_plt.imshow(cloud, interpolation="bilinear")
 
     def plot_btm(self, file):
-        test_file = os.path.join(wiki_path, file)
+        test_file = os.path.join(wiki_path, file.replace(' ', '_') + '.txt')
         wc = wordCloud(test_file)
         cloud = wc.getCloud()
+        self.btm_plt.set_title(file)
         self.btm_plt.imshow(cloud, interpolation="bilinear")
 
 
@@ -53,19 +55,22 @@ class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.left = 800
-        self.top = 500
-        self.width = 800
-        self.height = 600
+
+        # Get article fingerprints from database
+        db = Database()
+        self.db_sigs = db.get_all_signatures()
+        db.close()
+
+        # Initialize window
         self.title = 'Plagiarism detection'
         self.icon = QIcon(os.path.join(path, 'Wikipedia-icon.png'))
         self.initUI()
         self.show()
 
-    def initUI(self):
+    def initUI(self, left=800, top=500, width=600, height=400):
         # Window properties
+        self.setGeometry(left, top, width, height)
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
         self.setWindowIcon(self.icon)
 
         # Box layouts
@@ -91,13 +96,11 @@ class App(QWidget):
 
         # Table
         self.table = QTableWidget(self)
-        self.table.setColumnCount(2)
         self.table.verticalHeader().setVisible(False)
+        self.table.horizontalHeader().setVisible(False)
         self.table.setEditTriggers(QTableWidget.NoEditTriggers)
-        header = self.table.horizontalHeader()
-        header.setVisible(False)
-        header.setSectionResizeMode(0, QHeaderView.Stretch)
-        header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
+        self.table.setColumnCount(1)
+        self.table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
         self.table.clicked.connect(self.table_click)
         self.v_layout.addWidget(self.table)
 
@@ -112,61 +115,71 @@ class App(QWidget):
 
     @pyqtSlot()
     def on_click(self):
-        # Reset table
-        self.table.setRowCount(1)
-        self.table.setItem(0, 1, QTableWidgetItem(''))
+        # Reset table and wordclouds
+        self.reset_view()
 
-        # Create fingerprint of input file
-        self.file = self.textbox.text() + '.txt'
-        test_file = os.path.join(test_path, self.file)
+        # Detect potential plagiarised articles
+        file = self.textbox.text() + '.txt'
+        test_file = os.path.join(test_path, file)
         try:
-            test_fp = Fingerprint(test_file)
-            test_sig = test_fp.getSignatures()
-            test_substring = test_fp.getSubstring()
+            self.detect_plagiarism(test_file)
         except FileNotFoundError:
             self.table.setItem(0, 0, QTableWidgetItem('File not found'))
             return
-
-        # Get article fingerprints from database
-        db = Database()
-        db_sigs = db.get_all_signatures()
-        db.close()
-
-        # Detect potential plagiarism and show percentages on GUI
-        idx = 0
-        self.table.setColumnCount(2)
-        for name, sig in db_sigs.items():
-            sim = jaccard(test_sig, sig)
-            if sim >= 0.001:
-                substring = Fingerprint(os.path.join(wiki_path, name)).getSubstring()
-                real_sim = substring_match(test_substring, substring)
-                if real_sim > 0.01:
-                    self.table.setRowCount(idx + 1)
-                    self.table.setItem(idx, 0, QTableWidgetItem(name.replace('_', ' ').replace('.txt', '')))
-                    self.table.setItem(idx, 1, QTableWidgetItem('{} %'.format(round(real_sim * 100))))
-                    self.table.item(idx, 1).setTextAlignment(Qt.AlignRight|Qt.AlignVCenter)
-                    idx += 1
-        if idx == 0:
-            self.table.setItem(0, 0, QTableWidgetItem('No matches'))
+        
+        # Show detected articles on GUI
+        self.update_table()
 
         # Generate wordcloud from input article
-        self.m.setParent(None)
-        self.m = PlotCanvas(self)
-        self.h_layout.addWidget(self.m)
-        self.m.plot_top(self.file)
+        self.m.plot_top(file)
         self.m.draw()
 
     @pyqtSlot()
     def table_click(self):
         try:
-            file = self.table.currentItem().text().replace(' ', '_') + '.txt'
-            # self.m.setParent(None)
-            # self.m = PlotCanvas(self, self.file, selected_item)
-            # self.h_layout.addWidget(self.m)
+            file = self.table.currentItem().text()
             self.m.plot_btm(file)
             self.m.draw()
         except FileNotFoundError:
             pass
+
+    def reset_view(self):
+        self.table.setRowCount(1)
+        self.table.setColumnCount(1)
+        self.m.setParent(None)
+        self.m = PlotCanvas(self)
+        self.h_layout.addWidget(self.m)
+
+    def update_table(self):
+        if self.detected != []:
+            self.table.setColumnCount(2)
+            self.table.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeToContents)
+            for idx, (name, sim) in enumerate(self.detected):
+                self.table.setRowCount(idx + 1)
+                self.table.setItem(idx, 0, QTableWidgetItem(name.replace('_', ' ').replace('.txt', '')))
+                self.table.setItem(idx, 1, QTableWidgetItem('{} %'.format(round(sim * 100))))
+                self.table.item(idx, 1).setTextAlignment(Qt.AlignRight|Qt.AlignVCenter)
+        else:
+            self.table.setItem(0, 0, QTableWidgetItem('No matches'))
+
+    def detect_plagiarism(self, file):
+        # Create fingerprint of input file
+        test_fp = Fingerprint(file)
+        test_sig = test_fp.getSignatures()
+        test_substring = test_fp.getSubstring()
+
+        # Detect potential plagiarised articles
+        self.detected = []
+        for name, sig in self.db_sigs.items():
+            # Calculate jaccard similarity
+            sim = jaccard(test_sig, sig)
+            if sim >= 0.001:
+                # Calculate substring similarity
+                substring = Fingerprint(os.path.join(wiki_path, name)).getSubstring()
+                real_sim = substring_match(test_substring, substring)
+                if real_sim > 0.01:
+                    self.detected.append((name, real_sim))
+        self.detected.sort(key=lambda x: x[1], reverse=True)
         
 
 if __name__ == "__main__":
